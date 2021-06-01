dc8d7d3a0c10d12423b157907836d60b8cfb8468
mrcchef <201951003@iiitvadodara.ac.in>
2021-06-01 16:22:43 +0530

    minor changes in upcoming contest screen UI

--- lib/presentation/journeys/upcoming_contests/contest_card.dart
+++ lib/presentation/journeys/upcoming_contests/contest_card.dart
@@ -17,55 +17,43 @@ class ContestCard extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: Sizes.dimen_4.h,
-      ),
-      child: InkWell(
-        onTap: () {},
-        child: Card(
-          //elevation: 5,
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: Sizes.dimen_100.w,
+        padding: EdgeInsets.symmetric(
+          vertical: Sizes.dimen_12.w,
+          horizontal: Sizes.dimen_14.w,
+        ),
+        decoration: BoxDecoration(
           color: color,
-          //shadowColor: Colors.green.shade900,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(Sizes.dimen_24.w),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  //horizontal: Sizes.dimen_4.w,
-                  vertical: Sizes.dimen_4.h,
+          borderRadius: BorderRadius.circular(Sizes.dimen_24.w),
+        ),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6
+                  .copyWith(fontSize: Sizes.dimen_16.w),
+              textAlign: TextAlign.center,
+            ),
+            Spacer(),
+            Row(
+              children: [
+                Text(
+                  time,
+                  style: Theme.of(context).textTheme.subtitle1,
                 ),
-                child: Text(
-                  title,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      .copyWith(fontSize: Sizes.dimen_16.w),
+                Spacer(),
+                Text(
+                  date,
+                  style: Theme.of(context).textTheme.subtitle1,
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.dimen_16.w,
-                  vertical: Sizes.dimen_4.h,
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      time,
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                    Spacer(),
-                    Text(
-                      date,
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
