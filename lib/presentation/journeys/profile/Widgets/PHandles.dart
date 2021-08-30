ae6cf83016176b2d525df2f3bba2376bb0518f0d
sahilpotdukhe <sahilpotdukhe.ssp@gmail.com>
2021-08-30 11:47:49 +0530

    reduced profile page to widget

---
+++ lib/presentation/journeys/profile/Widgets/PHandles.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hexcolor/hexcolor.dart';
+class PHandles extends StatelessWidget {
+  const PHandles({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: HexColor('FDD9D9'),
+              borderRadius: BorderRadius.circular(30)),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 0),
+                child: Text(
+                  'Handles',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+              ListTile(
+                minVerticalPadding: 0,
+                title: Text(
+                  "CodeForces",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: HexColor('0B2FB0')),
+                ),
+                subtitle: Text(
+                  "sahilpotdukhe11",
+                  style: TextStyle(color: Colors.black),
+                ),
+                trailing: TextButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      FontAwesomeIcons.checkCircle,
+                      color: Colors.green,
+                      size: 20,
+                    ),
+                    label: Text(
+                      'Verified',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold),
+                    )),
+              ),
+              ListTile(
+                minVerticalPadding: 0,
+                title: Text(
+                  "CodeChef",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: HexColor('0B2FB0')),
+                ),
+                subtitle: Text(
+                  "sahilpotdukhe123",
+                  style: TextStyle(color: Colors.black),
+                ),
+                trailing: TextButton.icon(
+                  onPressed: () {},
+                  label: Text(
+                    'Verify',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  icon: Icon(
+                    FontAwesomeIcons.questionCircle,
+                    color: Colors.red,
+                    size: 20,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "HackerEarth",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: HexColor('0B2FB0')),
+                ),
+                subtitle: Text(
+                  "sahilpotdukhe142",
+                  style: TextStyle(color: Colors.black),
+                ),
+                trailing: TextButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      FontAwesomeIcons.checkCircle,
+                      color: Colors.green,
+                      size: 20,
+                    ),
+                    label: Text(
+                      'Verified',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold),
+                    )),
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/presentation/journeys/profile/Widgets/PUserAbout.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hexcolor/hexcolor.dart';
+class PUserAbout extends StatelessWidget {
+  const PUserAbout({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+            decoration: BoxDecoration(
+                color: HexColor('FDE5D9'),
+                borderRadius: BorderRadius.circular(30)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Student/Professional',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text('Student'),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding:
+                      const EdgeInsets.fromLTRB(8.0, 20, 10, 10),
+                      child: Icon(FontAwesomeIcons.laptop),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0, 0, 5),
+                  child: Text('Institution',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0, 0, 5),
+                  child: Text(
+                      'Indian Institute of Information Technology Vadodara'),
+                ),
+                SizedBox(
+                  height: 10,
+                )
+              ],
+            )));
+  }
+}
